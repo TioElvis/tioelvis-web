d0b23e8a2d45922705af3f94368824044d87aa44
TioElvis <elvis.veramijares@gmail.com>
2026-03-18 12:30:04 +0100

    Refactor ModeToggle component to accept className and props for better customization

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -2,16 +2,20 @@
 import { useTheme } from "next-themes";
 import { IconMoon, IconSun } from "@tabler/icons-react";
 
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 
-export function ModeToggle() {
+type Props = React.HTMLAttributes<HTMLButtonElement>;
+
+export function ModeToggle({ className, ...props }: Props) {
   const { resolvedTheme, setTheme } = useTheme();
 
   return (
     <Button
       variant="secondary"
-      className="h-10 w-10 rounded-full border"
+      className={cn("w-8 h-8 md:h-10 md:w-10 rounded-full border", className)}
       onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+      {...props}
       suppressHydrationWarning>
       <IconSun className="size-4 dark:block hidden" suppressHydrationWarning />
       <IconMoon className="size-4 dark:hidden block" suppressHydrationWarning />
