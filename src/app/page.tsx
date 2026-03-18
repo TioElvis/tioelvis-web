62633dae0e3a2a831d0d9700c6220275f7fe2893
TioElvis <elvis.veramijares@gmail.com>
2026-03-18 15:31:23 +0100

    Refactor Page component structure; replace Fragment with div and reorganize imports for clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
-import { Fragment } from "react/jsx-runtime";
-
-import { findProjects } from "@/lib/project";
+import Link from "next/link";
 
 import { Hero } from "./components/hero";
 import { About } from "./components/about";
 import { Navbar } from "./components/navbar";
 import { Contact } from "./components/contact";
 import { Projects } from "./components/projects";
+
+import { isAuthenticated } from "@/lib/auth";
+import { findProjects } from "@/lib/project";
+
+import { Button } from "@/components/ui/button";
 
 export default async function Page() {
   const { data, success } = await findProjects("?limit=3");
@@ -15,13 +18,20 @@ export default async function Page() {
     return <div>Error</div>;
   }
 
+  const isAuth = await isAuthenticated();
+
   return (
-    <Fragment>
+    <div className="relative">
       <Navbar />
       <Hero />
       <About />
       <Projects projects={data} />
       <Contact />
-    </Fragment>
+      {isAuth && (
+        <Button className="fixed bottom-8 right-8 z-50" asChild>
+          <Link href="/dashboard">Go to Dashboard</Link>
+        </Button>
+      )}
+    </div>
   );
 }
